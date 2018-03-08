4192b33b94a787d3bd7b2d2fffa9b2ab7eea6d9c
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-08 20:04:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -90,13 +90,13 @@ def list_songs_by_genre
 end
 
 def play_song
-  puts "Which song number would you like to play?"
-  input = gets.strip.to_i + 1
-  songs = Song.all.sort_by!{|song| song.name[0]}
-  song = songs[input]
-  puts "Playing #{song.name} by #{song.artist.name}"
-  else
-    nil
+  input = nil 
+  until input.to_i > 0 && input.to_i <= Song.all.length
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i + 1
+    songs = Song.all.sort_by!{|song| song.name[0]}
+    song = songs[input]
+    puts "Playing #{song.name} by #{song.artist.name}"
   end
 end
 
