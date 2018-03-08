cd64d4cb629a61fb4890bcf829d4455844171b75
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-08 20:02:10 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 class MusicLibraryController
   attr_accessor :path
 
@@ -93,7 +93,6 @@ def play_song
   puts "Which song number would you like to play?"
   input = gets.strip.to_i + 1
   songs = Song.all.sort_by!{|song| song.name[0]}
-  binding.pry
   if input > 0 && input <= songs.length
     puts "Playing #{songs[input].name} by #{songs[input].artist.name}"
   else
